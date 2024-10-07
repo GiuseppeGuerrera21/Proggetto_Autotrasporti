394fa5614aee728050bf5c62e7533b5e32fe1d1f
Giuseppe Guerrera <giuseppeguerrera21@gmail.com>
2024-10-07 14:26:53 +0200

    Impostazione progetto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<x-layout>
+    <x-nav/>
+    <div class="container-fluid mt-5 pt-5">
+        <div class="row">
+            <div class="col-6 ps-5 ">
+                <h1>Serviamo clienti in tutta Italia</h1>
+            </div>
+            <div class="col-6 pe-5  text-end mt-5">
+                <h6>Trasporti sicuri e consegne puntuali.</h6>
+            </div>       
+        </div>
+    </div>    
+        <div class="container-fluid ps-5 pe-5">
+            <video class="video" autoplay>
+                <source src="{{ asset('media/Video for web.mp4') }}" type="video/mp4">
+                Il tuo browser non supporta il video.
+            </video>
+        </div>
+</x-layout>
+
